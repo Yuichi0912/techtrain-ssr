32c132e55ed4cebb3b04f0163bfb92e26e85a3e3
Yuichi0912 <icec0912@gmail.com>
2023-02-09 20:04:10 +0900

    third commit

--- ssr/src/App.tsx
+++ ssr/src/App.tsx
@@ -1,12 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 export const App = () => {
+  const name: string = "森 祐一";
+  const profile: string = "実務未経験ですが頑張ります!";
+
   return (
     <div>
       <main>
-        <h1>ポートフォリオ</h1>
+        <h1>portfolio</h1>
+        <Card sx={{ maxWidth: 400, height: 500 }}>
+          <CardContent>
+            <Avatar alt="Cindy Baker" src="" sx={{ width: 56, height: 56 }} />
+            <Typography variant="h4" component="div">
+              {name}
+            </Typography>
+            <Typography variant="body2">{profile}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button variant="outlined">WORKS</Button>
+          </CardActions>
+        </Card>
       </main>
     </div>
   );
